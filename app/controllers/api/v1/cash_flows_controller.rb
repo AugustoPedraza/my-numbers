257411a41f4766fa8969f7f6c6c9cae3d553767a
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-06-07 21:58:39 -0300

    Added new specs

--- app/controllers/api/v1/cash_flows_controller.rb
+++ app/controllers/api/v1/cash_flows_controller.rb
@@ -2,8 +2,10 @@ class Api::V1::CashFlowsController < ApplicationController
   def create
     account = Account.find(params[:account_id])
 
-    account.cash_flows.build(params[:cash_flow])
+    cash_flow = account.cash_flows.build(params[:cash_flow])
+    cash_flow.save
 
-    account.save
+    response.status   = :created
+    response.location = api_v1_account_cash_flow_path(account, cash_flow)
   end
 end
--- spec/controllers/api/v1/cash_flows_controller_spec.rb
+++ spec/controllers/api/v1/cash_flows_controller_spec.rb
@@ -4,9 +4,14 @@ describe Api::V1::CashFlowsController, type: :controller do
   describe ".create" do
     context 'with valid attributes' do
       it "create a new cash flow" do
+        cash_flow_double = double(CashFlow)
+        allow(cash_flow_double).to receive(:save).and_return(true)
+        allow(cash_flow_double).to receive(:to_param).and_return(1)
+
         account_double  = double(Account)
-        allow(account_double).to receive_message_chain(:cash_flows, :build)
-        allow(account_double).to receive(:save).and_return(true)
+        allow(account_double).to receive(:to_param).and_return(33)
+        allow(account_double).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
+
 
         Account.stub(:find).with("33").and_return(account_double)
 
@@ -15,9 +20,48 @@ describe Api::V1::CashFlowsController, type: :controller do
         post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
 
         expect(account_double.cash_flows).to have_received(:build).with(cash_flow_json)
-        expect(account_double).to have_received(:save)
+        expect(cash_flow_double).to have_received(:save)
       end
-      it "return created resource code"
+
+      it "return created resource code" do
+        cash_flow_double = double(CashFlow)
+        allow(cash_flow_double).to receive(:save).and_return(true)
+        allow(cash_flow_double).to receive(:to_param).and_return(1)
+
+        account_double  = double(Account)
+        allow(account_double).to receive(:to_param).and_return(33)
+
+        allow(account_double).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
+
+
+        Account.stub(:find).with("33").and_return(account_double)
+
+        cash_flow_json = { :amount => 33.66, date: DateTime.now, description: 'fake description' }
+
+        post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it "return location for new created resource" do
+        cash_flow_double = double(CashFlow)
+        allow(cash_flow_double).to receive(:save).and_return(true)
+        allow(cash_flow_double).to receive(:to_param).and_return(1)
+
+        account_double  = double(Account)
+        allow(account_double).to receive(:to_param).and_return(33)
+
+        allow(account_double).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
+
+
+        Account.stub(:find).with("33").and_return(account_double)
+
+        cash_flow_json = { :amount => 33.66, date: DateTime.now, description: 'fake description' }
+
+        post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
+
+        expect(response.location).to eql('/api/v1/accounts/33/cash_flows/1')
+      end
     end
   end
 end
