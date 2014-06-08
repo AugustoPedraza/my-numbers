b634145a8e3face58730dd8bbdf07a35c46eb79c
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-06-08 18:34:56 -0300

    Added error handling

--- app/controllers/api/v1/cash_flows_controller.rb
+++ app/controllers/api/v1/cash_flows_controller.rb
@@ -1,20 +1,27 @@
 class Api::V1::CashFlowsController < ApplicationController
   def create
-    account = Account.find(params[:account_id])
-    cash_flow = account.cash_flows.build(cash_flow_params)
+    begin
+      account = Account.find(params[:account_id])
+      cash_flow = account.cash_flows.build(cash_flow_params)
 
-    if cash_flow.save
-      response.status   = :created
-      response.location = api_v1_account_cash_flow_path(account, cash_flow)
-    else
-      errors = {}
+      if cash_flow.save
+        response.status   = :created
+        response.location = api_v1_account_cash_flow_path(account, cash_flow)
+      else
+        errors = {}
 
-      cash_flow.errors.keys.each do |attr|
-        errors[attr] = cash_flow.errors.full_messages_for(attr)
+        cash_flow.errors.keys.each do |attr|
+          errors[attr] = cash_flow.errors.full_messages_for(attr)
+        end
+
+        response.status = :unprocessable_entity
+        render json: errors
       end
-
-      render json: errors
+    rescue ActiveRecord::RecordNotFound => e
+      response.status = :not_found
+      render json: "Account not found"
     end
+
   end
 
   private
--- spec/controllers/api/v1/cash_flows_controller_spec.rb
+++ spec/controllers/api/v1/cash_flows_controller_spec.rb
@@ -2,90 +2,112 @@ require 'spec_helper'
 
 describe Api::V1::CashFlowsController, type: :controller do
   describe ".create" do
-
     let(:cash_flow_json) do
-        {
-          :amount => 33.66,
-          date: DateTime.now,
-          description: 'fake description'
-        }
+      {
+        :amount => 33.66,
+        date: DateTime.now,
+        description: 'fake description'
+      }
     end
 
-    let(:account_double) do
-      obj  = double(Account)
-      allow(obj).to receive(:to_param).and_return(33)
-      allow(obj).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
-
-      obj
-    end
-
-    before(:each) do
-      Account.stub(:find).with("33").and_return(account_double)
-      post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
-    end
-
-    context 'with valid attributes' do
-      let(:cash_flow_double) do
-        obj = double(CashFlow)
-        allow(obj).to receive(:save).and_return(true)
-        allow(obj).to receive(:to_param).and_return(1)
+    context 'with valid account id' do
+      let(:account_double) do
+        obj  = double(Account)
+        allow(obj).to receive(:to_param).and_return(33)
+        allow(obj).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
 
         obj
       end
 
-      it "create a new cash flow" do
-        expect(cash_flow_double).to have_received(:save)
+      before(:each) do
+        Account.stub(:find).with("33").and_return(account_double)
+        post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
       end
 
-      it "return created resource code" do
-        expect(response).to have_http_status(:created)
-      end
+      context 'with valid attributes' do
+        let(:cash_flow_double) do
+          obj = double(CashFlow)
+          allow(obj).to receive(:save).and_return(true)
+          allow(obj).to receive(:to_param).and_return(1)
 
-      it "return location for new created resource" do
-        expect(response.location).to eql('/api/v1/accounts/33/cash_flows/1')
-      end
-
-      it "return a empty body" do
-        expect(response.body).to eql("")
-      end
-    end
-
-    context 'with invalid attributes' do
-      let(:errors) do
-        {
-          :description => ["Description can't blank"],
-          :amount => ["Amount can't be less than 0"],
-          :payee => ["payee can't be blank", "other violated validation"]
-        }
-      end
-
-      let(:cash_flow_double) do
-        allow_message_expectations_on_nil
-
-        obj = double(CashFlow)
-        allow(obj).to receive(:save).and_return(false)
-        allow(obj).to receive(:to_param).and_return(1)
-        allow(obj).to receive(:errors)
-
-        allow(obj).to receive_message_chain(:errors, :keys).and_return(errors.keys)
-
-        errors.each_pair do |attr, values|
-          obj.stub_chain(:errors, :full_messages_for).with(attr).and_return(values)
+          obj
         end
 
-        obj
+        it "create a new cash flow" do
+          expect(cash_flow_double).to have_received(:save)
+        end
+
+        it "return created resource code" do
+          expect(response).to have_http_status(:created)
+        end
+
+        it "return location for new created resource" do
+          expect(response.location).to eql('/api/v1/accounts/33/cash_flows/1')
+        end
+
+        it "return a empty body" do
+          expect(response.body).to eql("")
+        end
       end
 
-      describe "validations error" do
-        it "return a json with all messages" do
-          expected =
+      context 'with invalid attributes' do
+        let(:errors) do
           {
             :description => ["Description can't blank"],
             :amount => ["Amount can't be less than 0"],
             :payee => ["payee can't be blank", "other violated validation"]
-          }.to_json
+          }
+        end
 
-          expect(response.body).to eql(expected)
+        let(:cash_flow_double) do
+          allow_message_expectations_on_nil
+
+          obj = double(CashFlow)
+          allow(obj).to receive(:save).and_return(false)
+          allow(obj).to receive(:to_param).and_return(1)
+          allow(obj).to receive(:errors)
+
+          allow(obj).to receive_message_chain(:errors, :keys).and_return(errors.keys)
+
+          errors.each_pair do |attr, values|
+            obj.stub_chain(:errors, :full_messages_for).with(attr).and_return(values)
+          end
+
+          obj
+        end
+
+        describe "validations error" do
+          it "return a json with all messages" do
+            expected =
+            {
+              :description => ["Description can't blank"],
+              :amount => ["Amount can't be less than 0"],
+              :payee => ["payee can't be blank", "other violated validation"]
+            }.to_json
+
+            expect(response.body).to eql(expected)
+          end
+
+          it "return unprocessable entity reponse code" do
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+        end
+      end
+    end
+
+    context 'with invalid account id' do
+      before(:each) do
+        Account.stub(:find).with("33").and_raise(ActiveRecord::RecordNotFound)
+        post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
+      end
+
+      context 'with invalid account' do
+        it "return a body message" do
+          expect(response.body).to eql("Account not found")
+        end
+
+        it "return 404 code" do
+          expect(response).to have_http_status(:not_found)
         end
       end
     end
