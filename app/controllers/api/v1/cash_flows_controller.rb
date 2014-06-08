f588ec50930e6c2f1e9a0363f381c0f6f43df252
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-06-08 17:50:15 -0300

    Added support to response violated validations

--- app/controllers/api/v1/cash_flows_controller.rb
+++ app/controllers/api/v1/cash_flows_controller.rb
@@ -1,12 +1,20 @@
 class Api::V1::CashFlowsController < ApplicationController
   def create
     account = Account.find(params[:account_id])
-
     cash_flow = account.cash_flows.build(cash_flow_params)
-    cash_flow.save
 
-    response.status   = :created
-    response.location = api_v1_account_cash_flow_path(account, cash_flow)
+    if cash_flow.save
+      response.status   = :created
+      response.location = api_v1_account_cash_flow_path(account, cash_flow)
+    else
+      errors = {}
+
+      cash_flow.errors.keys.each do |attr|
+        errors[attr] = cash_flow.errors.full_messages_for(attr)
+      end
+
+      render json: errors
+    end
   end
 
   private
--- spec/controllers/api/v1/cash_flows_controller_spec.rb
+++ spec/controllers/api/v1/cash_flows_controller_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 describe Api::V1::CashFlowsController, type: :controller do
   describe ".create" do
+
+    let(:cash_flow_json) do
+        {
+          :amount => 33.66,
+          date: DateTime.now,
+          description: 'fake description'
+        }
+    end
+
+    let(:account_double) do
+      obj  = double(Account)
+      allow(obj).to receive(:to_param).and_return(33)
+      allow(obj).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
+
+      obj
+    end
+
+    before(:each) do
+      Account.stub(:find).with("33").and_return(account_double)
+      post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
+    end
+
     context 'with valid attributes' do
       let(:cash_flow_double) do
         obj = double(CashFlow)
@@ -9,27 +31,6 @@ describe Api::V1::CashFlowsController, type: :controller do
         allow(obj).to receive(:to_param).and_return(1)
 
         obj
-      end
-
-      let(:account_double) do
-        obj  = double(Account)
-        allow(obj).to receive(:to_param).and_return(33)
-        allow(obj).to receive_message_chain(:cash_flows, :build).and_return(cash_flow_double)
-
-        obj
-      end
-
-      let(:cash_flow_json) do
-        {
-          :amount => 33.66,
-          date: DateTime.now,
-          description: 'fake description'
-        }
-      end
-
-      before(:each) do
-        Account.stub(:find).with("33").and_return(account_double)
-        post :create,  format: :json, :account_id => 33, cash_flow: cash_flow_json
       end
 
       it "create a new cash flow" do
@@ -42,6 +43,50 @@ describe Api::V1::CashFlowsController, type: :controller do
 
       it "return location for new created resource" do
         expect(response.location).to eql('/api/v1/accounts/33/cash_flows/1')
+      end
+
+      it "return a empty body" do
+        expect(response.body).to eql("")
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:errors) do
+        {
+          :description => ["Description can't blank"],
+          :amount => ["Amount can't be less than 0"],
+          :payee => ["payee can't be blank", "other violated validation"]
+        }
+      end
+
+      let(:cash_flow_double) do
+        allow_message_expectations_on_nil
+
+        obj = double(CashFlow)
+        allow(obj).to receive(:save).and_return(false)
+        allow(obj).to receive(:to_param).and_return(1)
+        allow(obj).to receive(:errors)
+
+        allow(obj).to receive_message_chain(:errors, :keys).and_return(errors.keys)
+
+        errors.each_pair do |attr, values|
+          obj.stub_chain(:errors, :full_messages_for).with(attr).and_return(values)
+        end
+
+        obj
+      end
+
+      describe "validations error" do
+        it "return a json with all messages" do
+          expected =
+          {
+            :description => ["Description can't blank"],
+            :amount => ["Amount can't be less than 0"],
+            :payee => ["payee can't be blank", "other violated validation"]
+          }.to_json
+
+          expect(response.body).to eql(expected)
+        end
       end
     end
   end
